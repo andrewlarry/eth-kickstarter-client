8ade9ea8da3ccee3d82a3c91a63cac25876a7143
Andrew <afuselier23@gmail.com>
2018-08-07 15:27:19 -0700

    refactor to account for SSR

--- eth/web3.js
+++ eth/web3.js
@@ -1,7 +1,16 @@
 import Web3 from 'web3';
 
-// window.web3.currentProvider is injected by metamask
-// *** Need to refactor to account for non metamask users
-const web3 = new Web3(window.web3.currentProvider);
+import { TEST_ENDPOINT } from '../config/infura';
+
+// Next.js does SSR, so window is unavailable
+let web3;
+if (typeof window !== undefined && window.web3 !== undefined) {
+  // window.web3.currentProvider is injected by metamask
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // Running on server or user not using metamask
+  const provider = new Web3.provider.HttpProvider(TEST_ENDPOINT);
+  web3 = new Web3(provider);
+}
 
 export default web3;
