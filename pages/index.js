06201ebb0ea42e0724449dc3697a0218465c6068
Andrew <afuselier23@gmail.com>
2018-08-07 13:07:33 -0700

    Creating the root index.js route file

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// The index.js page in a Next application is the component that gets
+// delivered when the root route ('/') is requested
+
+import React from 'react';
+
+export default () => {
+  return (
+    <h1>New Campaign</h1>
+  );
+}
