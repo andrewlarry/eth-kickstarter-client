d2bb8cbb38d2430b19159cf31dbe65284e6c74f8
Andrew <afuselier23@gmail.com>
2018-08-08 10:02:41 -0700

    refactor to work without injected web3 from metamask

--- eth/web3.js
+++ eth/web3.js
@@ -4,12 +4,20 @@ import { TEST_ENDPOINT } from '../config/infura';
 
 // Next.js does SSR, so window is unavailable
 let web3;
-if (typeof window !== undefined && window.web3 !== undefined) {
+
+// Weird work around because typeof if not working correctly
+let window = window || undefined;
+let injected = false;
+if (window !== undefined) {
+  if (window.web !== undefined) injected = true;
+}
+
+if (window !== undefined && injected) {
   // window.web3.currentProvider is injected by metamask
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // Running on server or user not using metamask
-  const provider = new Web3.provider.HttpProvider(TEST_ENDPOINT);
+  const provider = new Web3.providers.HttpProvider(TEST_ENDPOINT);
   web3 = new Web3(provider);
 }
 
