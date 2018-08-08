c332fbfb81cc2ded49e4adde1cc44ddbb7fc3185
Andrew <afuselier23@gmail.com>
2018-08-08 12:50:14 -0700

    finialize the /campaigns/new page

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
+import factory from '../../eth/factory';
+import web3 from '../../eth/web3';
+
 
 class CampaignNew extends Component {
+  state = {
+    minimumContribution: '',
+    errorMessage: ''
+  };
+
+  // Use this syntax to prevent binding 'this'
+  onSubmit = async (event) => {
+    // Prevent form submission
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    try {
+      await factory.methods.createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch(err) {
+      this.setState({ errorMessage: err.message });
+    }
+  };
+
   render() {
     return (
       <Layout>
-        <h1>Hi</h1>
+        <h3>Create a Campaign</h3>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="Wei" 
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event => this.setState({ minimumContribution: event.target.value })}
+            />
+          </Form.Field>
+          <Message 
+            error
+            header="Error!"
+            content={this.state.errorMessage}
+          />
+          <Button primary>Create</Button>
+        </Form>
       </Layout>
     );
   }
