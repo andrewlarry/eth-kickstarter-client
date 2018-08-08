03da2b3d3c528e677a811503334ba04462a7dceb
Andrew <afuselier23@gmail.com>
2018-08-08 12:03:52 -0700

    adding the CDN link to the Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react'
+import Head from 'next/head';
 
 import Header from './Header';
 
 export default ({ children }) => {
   return (
     <Container>
+      {/* Next.js doesn't have out of the box support for css modules. Add the CDN to the head tag. */}
+      <Head>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"></link>
+      </Head>
       <Header />
       {children}
     </Container>
