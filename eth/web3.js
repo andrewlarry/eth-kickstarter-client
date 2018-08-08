088098610dd2e8b928916bbc1663b5ffbf9d2519
Andrew <afuselier23@gmail.com>
2018-08-08 12:37:05 -0700

    refactor the 'window' reference to be a 'global' reference

--- eth/web3.js
+++ eth/web3.js
@@ -6,15 +6,15 @@ import { TEST_ENDPOINT } from '../config/infura';
 let web3;
 
 // Weird work around because typeof if not working correctly
-let window = window || undefined;
 let injected = false;
-if (window !== undefined) {
-  if (window.web !== undefined) injected = true;
+
+if (typeof global !== 'undefined') {
+  if (global.web3 !== undefined) injected = true;
 }
 
-if (window !== undefined && injected) {
+if (typeof global !== 'undefined' && injected) {
   // window.web3.currentProvider is injected by metamask
-  web3 = new Web3(window.web3.currentProvider);
+  web3 = new Web3(global.web3.currentProvider);
 } else {
   // Running on server or user not using metamask
   const provider = new Web3.providers.HttpProvider(TEST_ENDPOINT);
