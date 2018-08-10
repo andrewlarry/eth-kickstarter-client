062aed24abbe3661c12376318c07b27c78a0a305
Andrew <afuselier23@gmail.com>
2018-08-09 18:04:16 -0700

    finished the renderCards method

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 
 import Layout from '../../components/Layout';
 import Campaign from '../../eth/campaign.js';
+import web3 from '../../eth/web3';
 
 class CampaignShow extends Component {
 
@@ -22,10 +24,49 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards() {
+    const { minimumContribution, balance, requestCount, approversCount, manager } = this.props;
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description: 'The manager is the creator of campaign. The manager can create and finalize spending requests.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description: 'The minimum contributor in wei to become a campaign approver.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: requestCount,
+        meta: 'Number of Requests',
+        description: 'The number of spending requests the manager has issued.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description: 'The number of people that have donated to the campaign.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance (ether)',
+        description: 'The balance is the amount of fund the campaign has left to spend.',
+        style: { overflowWrap: 'break-word'}
+      }
+    ];
+
+    return <Card.Group items={items} />
+  }
+
   render() {
     return(
       <Layout>
         <h2>Campaign Page</h2>
+        {this.renderCards()}
       </Layout>
     );
   }
