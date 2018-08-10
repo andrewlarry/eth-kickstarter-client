9fa03e1d8c33312ff513c823634c3f942b52cc19
Andrew <afuselier23@gmail.com>
2018-08-09 17:47:19 -0700

    adding the getInitialProps function

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
+import Layout from '../../components/Layout';
+import Campaign from '../../eth/campaign.js';
 
 class CampaignShow extends Component {
+
+  static async getInitialProps({ query }) {
+    // web3 contract instance 
+    const campaign = Campaign(query.address);
+
+    const summary = await campaign.methods.getSummary().call()
+    console.log(summary);
+    return {};
+  }
+
   render() {
-    return <h3>Campaign</h3>
+    return(
+      <Layout>
+        <h2>Campaign Page</h2>
+      </Layout>
+    );
   }
 }
 
