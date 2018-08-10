0c5809fbd689e9ced25a2738e6f845cea89bd5f1
Andrew <afuselier23@gmail.com>
2018-08-09 17:40:18 -0700

    added compiled Campaign contract and fixed import reference error

--- eth/campaign.js
+++ eth/campaign.js
@@ -1,5 +1,5 @@
 import web3 from './web3';
-import Campaign from './build/CampaignFactory.json';
+import Campaign from './Campaign.json';
 
 export default (address) => {
   return new web3.eth.Contract(
