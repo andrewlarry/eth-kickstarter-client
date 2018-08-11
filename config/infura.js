11f48288b367c22715b8adc7c0b7c2dee420aa6f
Andrew <afuselier23@gmail.com>
2018-08-10 18:30:10 -0700

    adding files for herkou

---
+++ config/infura.js
@@ -0,0 +1,5 @@
+module.exports = {
+  TEST_ENDPOINT: 'https://rinkeby.infura.io/v3/8aa412e967aa49f2b475c687e4aba31a',
+  API_KEY: '8aa412e967aa49f2b475c687e4aba31a',
+  API_SECRET: '92d48385b5d44efda6f60b87f5d83a43'
+}
